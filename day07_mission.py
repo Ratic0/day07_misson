91a7979b48049bc3f72b619b697ac06325a6540b
KimTaeYeong <12170548@inha.edu>
2023-01-20 18:47:00 +0900

    day07.mission_10.5~10.7

--- day07_mission.py
+++ day07_mission.py
@@ -1,48 +1,35 @@
-#10.1
-class Thing:
-    def ex(self):
-        return self
-
-example = Thing()
-print(Thing)
-print(example)
-
-#10.2
-
-class Thing2:
-    def __init__(self):
-        self.letters = 'abc'
-    def outing(self):
-        print(self.letters)
-
-A = Thing2()
-A.outing()
-
-#10.3
-class Thing3:
-    def __init__(self):
-        self.letters = 0
-
-B=Thing3()
-B.letters = 'xyz'
-print(B.letters)
-
-#10.4
-class Element:
+#10.5
+el_dict = {'name' : 'Hydrogen', 'symbol' : 'H', 'number' : 1}
+class Element1():
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
-C = Element
-C.name = 'Hydrogen'
-C.symbol = 'H'
-C.number = 1
-print(f'{C.name}, {C.symbol}, {C.number}')
+l = list(el_dict.values())
+
+AA = Element1(l[0], l[1], l[2])
+print(f'10.5 : {AA.name} ,{AA.symbol}, {AA.number}')
 
 
 
+#10.6~7
+class Element2():
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+
+    def dump(self):
+        print(f'10.6 : {self.name}, {self.symbol}, {self.number}')
+
+    def __str__(self):
+        return f'10.7 : {self.name}, {self.symbol}, {self.number}'
 
 
 
+hydrogen = Element2('Kim','K',2)
+hydrogen.dump()
+hydrogen2 = Element2('No','N','3')
+print(hydrogen2)
 
-
+#10.8
